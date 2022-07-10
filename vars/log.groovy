a216dd5e317d59f19785b9a77c253220820ec0e8
Krishna Vamsi <nvgkvamsi@gmail.com>
2022-07-09 18:58:36 -0700

    updating ./roboshop-jenkins-shared-library

--- vars/log.groovy
+++ vars/log.groovy
@@ -5,3 +5,23 @@ def info(message) {
 def warning(message) {
     echo "WARNING: ${message}"
 }
+
+
+def call(){
+    pipeline {
+        agent any
+        stages {
+            stage('One') {
+                steps {
+                    sh 'echo One'
+                }
+            }
+            stage('Two') {
+                steps {
+                    sh 'echo Two'
+                }
+            }
+        }
+
+    }
+}
