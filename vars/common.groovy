1d94b936a6f8a77cac2ffe2c4cc26b963a874b6b
Krishna Vamsi <nvgkvamsi@gmail.com>
2022-07-10 15:44:39 -0700

    updating ./roboshop-jenkins-shared-library

--- vars/common.groovy
+++ vars/common.groovy
@@ -17,8 +17,8 @@ def publishArtifacts() {
     stage('Push Artifacts to Nexus'){
         withCredentials([usernamePassword(credentialsId: 'NEXUS', passwordVariable: 'pass', usernameVariable: 'user')]) {
             sh """
-      curl -v -u ${user}:${pass} --upload-file ${COMPONENT}-${TAG_NAME}.zip http://18.234.76.74:8081/repository/${COMPONENT}/${COMPONENT}-${TAG_NAME}.zip
-     """
+               curl -v -u ${user}:${pass} --upload-file ${COMPONENT}-${TAG_NAME}.zip http://18.234.76.74:8081/repository/${COMPONENT}/${COMPONENT}-${TAG_NAME}.zip
+            """
 
         }
 
