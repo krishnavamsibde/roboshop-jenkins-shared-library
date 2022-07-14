7e0aa039488e2f5f5f12dbd0fb617b7c16834172
Krishna Vamsi <nvgkvamsi@gmail.com>
2022-07-13 20:50:19 -0700

    updating ./roboshop-jenkins-shared-library

--- vars/common.groovy
+++ vars/common.groovy
@@ -47,8 +47,8 @@ def codeChecks(){
     stage('Quality Checks & Unit Tests') {
         parallel([
                 qualityChecks: {
-                    withCredentials([usernamePassword(credentialsId: 'SONAR', passwordVariable: 'pass', usernameVariable: 'user')])
-                    sh "sonar-scanner -Dsonar.projectKey=${COMPONENT} -Dsonar.host.url=http://18.213.151.115:9000 -Dsonar.login=${user} -Dsonar.password=${pass}"
+                    withCredentials([usernamePassword(credentialsId: 'SONAR', passwordVariable: 'pass', usernameVariable: 'user')]){
+                    sh "sonar-scanner -Dsonar.projectKey=${COMPONENT} -Dsonar.host.url=http://18.213.151.115:9000 -Dsonar.login=${user} -Dsonar.password=${pass}" }
                 },
                 unitTests: {
                     unitTests()
