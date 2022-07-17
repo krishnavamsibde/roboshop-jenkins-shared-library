f358b7559a5956504422283e8274da2d2b1894d2
Krishna Vamsi <nvgkvamsi@gmail.com>
2022-07-17 00:02:55 -0700

    updating ./roboshop-jenkins-shared-library

--- vars/common.groovy
+++ vars/common.groovy
@@ -9,30 +9,30 @@ def pipelineInit(){
 def publishArtifacts() {
     env.ENV = "dev"
     stage("Prepare Artifacts") {
-        if (env.APP_TYPE == "nodejs"){
-        sh """
+        if (env.APP_TYPE == "nodejs") {
+            sh """
             zip -r ${ENV}-${COMPONENT}-${TAG_NAME}.zip node_modules server.js
         """
         }
-        if (env.APP_TYPE == "maven"){
+        if (env.APP_TYPE == "maven") {
             sh """
             cp target/${COMPONENT}-1.0.jar ${COMPONENT}.jar
             zip -r ${ENV}-${COMPONENT}-${TAG_NAME}.zip ${COMPONENT}.jar
         """
         }
-        if (env.APP_TYPE == "python"){
+        if (env.APP_TYPE == "python") {
             sh """
             zip -r ${ENV}-${COMPONENT}-${TAG_NAME}.zip *.py ${COMPONENT}.ini requirements.txt
         """
         }
-        if (env.APP_TYPE == "nginx"){
+        if (env.APP_TYPE == "nginx") {
             sh """
             cd static
             zip -r ../${ENV}-${COMPONENT}-${TAG_NAME}.zip *
         """
         }
     }
-    stage('Push Artifacts to Nexus'){
+    stage('Push Artifacts to Nexus') {
         withCredentials([usernamePassword(credentialsId: 'NEXUS', passwordVariable: 'pass', usernameVariable: 'user')]) {
             sh """
                curl -v -u ${user}:${pass} --upload-file ${ENV}-${COMPONENT}-${TAG_NAME}.zip http://172.31.1.200:8081/repository/${COMPONENT}/${ENV}-${COMPONENT}-${TAG_NAME}.zip
@@ -42,11 +42,26 @@ def publishArtifacts() {
 
     }
 
-    stage('Deploy to Dev Env'){
+    stage('Deploy to Dev Env') {
         build job: 'deploy-to-any-env', parameters: [string(name: 'COMPONENT', value: "${COMPONENT}"), string(name: 'ENV', value: "${ENV}"), string(name: 'APP_VERSION', value: "${TAG_NAME}")]
     }
 
+    stage('Run Smoke Tests') {
+        sh "echo smoke tests"
+
+    }
+    promoteRelease("dev","qa")
 }
+    promoteRelease(SOURCE_ENV, TARGET_ENV) {
+        withCredentials([usernamePassword(credentialsId: 'NEXUS', passwordVariable: 'pass', usernameVariable: 'user')]) {
+            sh """
+               cp ${SOURCE_ENV}-${COMPONENT}-${TAG_NAME}.zip cp ${ENV}-${COMPONENT}-${TAG_NAME}.zip
+               curl -v -u ${user}:${pass} --upload-file ${ENV}-${COMPONENT}-${TAG_NAME}.zip http://172.31.1.200:8081/repository/${COMPONENT}/${ENV}-${COMPONENT}-${TAG_NAME}.zip
+            """
+
+        }
+
+    }
 
 
 def codeChecks(){
