f0d2d875b60913a4ce9fa915e1389305d556ce08
Krishna Vamsi <nvgkvamsi@gmail.com>
2022-08-04 00:27:40 -0700

    updating ./roboshop-jenkins-shared-library

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -2,28 +2,29 @@ def call() {
     node {
         properties([
                 parameters([
-                        choice(choices: ['dev', 'prod'],  description: "Choose Environment", name: "ENV"),
-                        choice(choices: ['apply', 'destroy'],  description: "Choose Action", name: "ENV"),
+                        choice(choices: ['dev', 'prod'], description: "Choose Environment", name: "ENV"),
+                        choice(choices: ['apply', 'destroy'], description: "Choose Action", name: "ACTION"),
                 ])
         ])
-        ansiColor('xterm'){
+
+        ansiColor('xterm') {
 
             stage('Code Checkout') {
-                    sh 'find . | sed -e "1d" | xargs rm -rf'
-                    git branch: 'main', url: 'https://github.com/krishnavamsi7616/roboshop-terraform-mutable.git'
-                }
+                sh 'find . | sed -e "1d" | xargs rm -rf'
+                git branch: 'main', url: 'https://github.com/krishnavamsi7616/roboshop-terraform-mutable.git'
+            }
 
-        stage('Terraform INIT') {
-            sh 'terraform init -backend-config=env/${ENV}-backend.tfvars'
-        }
+            stage('Terraform INIT') {
+                sh 'terraform init -backend-config=env/${ENV}-backend.tfvars'
+            }
 
-        stage('Terraform Plan'){
-            sh 'terraform plan -var-file=env/${ENV}.tfvars'
-        }
+            stage('Terraform Plan') {
+                sh 'terraform plan -var-file=env/${ENV}.tfvars'
+            }
 
-        stage('Terraform ${ACTION}'){
-            sh 'terraform ${ACTION} -auto-approve -var-file=env/${ENV}.tfvars'
+            stage("Terraform ${ACTION}") {
+                sh 'terraform ${ACTION} -auto-approve -var-file=env/${ENV}.tfvars'
+            }
         }
-    }
     }
 }
